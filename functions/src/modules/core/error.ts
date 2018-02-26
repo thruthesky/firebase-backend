a427a11c6d1c772217a8ffbb4c4abeba59867afa
gemlorenz10 <gemlorenz10@gmail.com>
2018-02-26 21:03:27 +0800

    update

--- functions/src/modules/core/error.ts
+++ functions/src/modules/core/error.ts
@@ -3,12 +3,16 @@ export const NO_EMAIL = -50; es[NO_EMAIL] = 'No email address.';
 export const NO_PASSWORD = -51; es[NO_PASSWORD] = 'No password.';
 export const NO_NAME = -52; es[NO_NAME] = 'No name.';
 export const NO_ID = -53; es[NO_ID] = 'No uid.';
-export const WRONG_GENDER = -61; es[WRONG_GENDER] = 'Wrong gender.';
+export const WRONG_GENDER = -54; es[WRONG_GENDER] = 'Wrong gender.';
+
 export const WRONG_ROUTE = -60; es[WRONG_ROUTE] = 'The given route is not exists. It is a wrong route.';
 export const EMPTY_ROUTE = -61; es[EMPTY_ROUTE] = 'Empty route.';
 export const WRONG_METHOD = -61; es[WRONG_METHOD] = 'Wrong method.';
+
 export const NO_USER_DOCUMENT_ID = -4010; es[NO_USER_DOCUMENT_ID] = 'Empty document path for user collection.';
-export const DOCUEMNT_ID_DOES_NOT_EXISTS_FOR_UPDATE = 5; es[DOCUEMNT_ID_DOES_NOT_EXISTS_FOR_UPDATE] = 'Document ID does not exsits for update.';
+export const DOCUMENT_ID_DOES_NOT_EXISTS_FOR_UPDATE = 5; es[DOCUMENT_ID_DOES_NOT_EXISTS_FOR_UPDATE] = 'Document ID does not exsits for update.';
+export const DOCUMENT_ID_DOES_NOT_EXISTS_FOR_GET = 6; es[DOCUMENT_ID_DOES_NOT_EXISTS_FOR_GET] = 'No data found for document id.';
+
 
 
 
